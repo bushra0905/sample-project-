de601369fda75082a3243a8b47e8a7e379d81725
bushra0905 <bushramalik0905@gmail.com>
2025-05-08 07:26:42 +0530

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,45 @@
+# DFS algorithm
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = set() 
+
+def dfs(visited, graph, node):  
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("Following is the Depth-First Search")
+dfs(visited, graph, '5')
+
+ 
+
+
+                                        Assignment No. 1
+TITLE :  Implement  Depth first search algorithm  and breadth first sear algorithm.    
+BFS implement
+Program :
+def bfs(visited, graph, node): 
+  visited.append(node)
+  queue.append(node)
+
+  while queue:          
+    m = queue.pop(0) 
+    print (m, end = " ") 
+
+    for neighbour in graph[m]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+print("Following is the Breadth-First Search")
+bfs(visited, graph, '5')    
+ 
